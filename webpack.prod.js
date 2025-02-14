3686903d380c3aeb8e49762dc5bddbc6148b18e6
Nelson Valero Barcelona <169047839+Nelvb@users.noreply.github.com>
2025-02-14 10:52:28 +0000

    Eliminando dependencia de BASENAME en Webpack

--- webpack.prod.js
+++ webpack.prod.js
@@ -9,7 +9,7 @@ module.exports = merge(common, {
     output: {
         filename: 'bundle.js',
         path: path.resolve(__dirname, 'dist'),
-        publicPath: '/'
+        publicPath: '/The_Roas_Factory/'
     },
     devServer: {
         historyApiFallback: true
